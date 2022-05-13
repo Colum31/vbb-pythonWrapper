77f268f6604b1b3ec88e44575f85e93bedff08bb
colum31 <19858862+colum31@users.noreply.github.com>
2022-05-14 00:25:57 +0200

    adding license and updating setup.py

--- setup.py
+++ setup.py
@@ -3,8 +3,10 @@ from setuptools import setup
 setup(
     name='vbbpy',
     version='1.1.5',
-    description='A python wrapper for the VBB REST-API',
-    author='Colum31',
+    description='A python wrapper for the VBB REST-API by derhuerst',
+    url="https://github.com/Colum31/vbb-pythonWrapper",
+    author='colum31',
+    license="MIT",
     packages=['vbbpy'],  # would be the same as name
     install_requires=['requests', 'datetime', 'geopy']  # external packages acting as dependencies
 )
