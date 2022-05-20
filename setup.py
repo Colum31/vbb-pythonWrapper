3f87b542c8a7267c2ad51266b8566cfe679230b0
colum31 <19858862+colum31@users.noreply.github.com>
2022-05-20 22:06:15 +0200

    examples are added as scripts

--- setup.py
+++ setup.py
@@ -2,12 +2,14 @@ from setuptools import setup
 
 setup(
     name='vbbpy',
-    version='1.1.5',
+    version='1.1.6',
     python_requires='>3.6',
     description='A python wrapper for the VBB REST-API by derhuerst',
     url="https://github.com/Colum31/vbb-pythonWrapper",
     author='colum31',
     license="MIT",
-    packages=['vbbpy'],  # would be the same as name
-    install_requires=['requests', 'datetime', 'geopy']  # external packages acting as dependencies
+    packages=['vbbpy'],
+    install_requires=['requests', 'datetime', 'geopy'],  # external packages acting as dependencies
+    scripts=['examples/vbbpy-stationId', 'examples/vbbpy-stationInformation', 'examples/vbbpy-showDepartures',
+             'examples/vbbpy-addressRouting']
 )
